82950bd6c99450929eb5f8f855d08dd8765d8da1
hwannee <doormoo2@naver.com>
2026-09-09 00:10:12 +0900

    feat(auth): add LoginRequest DTO

---
+++ IEUM_BE/ieum-auth/src/main/java/com/hwannee/ieum/auth/issue/web/dto/LoginRequest.java
@@ -0,0 +1,10 @@
+package com.hwannee.ieum.auth.issue.web.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+        @NotBlank @Email String email,
+        @NotBlank String password
+) {
+}
