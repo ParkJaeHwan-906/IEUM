d7a00bf7a6db7e63f39307bc8660457dd59f8e1b
hwannee <doormoo2@naver.com>
2026-09-12 00:42:54 +0900

    feat(api): enable OrderProperties binding

---
+++ IEUM_BE/ieum-api/src/main/java/com/hwannee/ieum/orders/config/OrderConfig.java
@@ -0,0 +1,9 @@
+package com.hwannee.ieum.orders.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(OrderProperties.class)
+public class OrderConfig {
+}
